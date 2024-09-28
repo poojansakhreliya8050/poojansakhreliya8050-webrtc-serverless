c48bfddcc640ecfa54d2d9c4863de2be6c87f476
Poojan Sakhreliya <POOJANSAKHRELIYA12@GMAIL.COM>
2024-09-28 20:24:33 +0530

    audio enable

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let localStream;
 let remoteStream;
 
 let init = async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:true})
     remoteStream = new MediaStream()
     document.getElementById('user-1').srcObject = localStream
     document.getElementById('user-2').srcObject = remoteStream
